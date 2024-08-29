895a960759585c403159e6a1d9a639372446699e
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-08-29 18:21:24 -0300

    feat: product routes

--- src/main/java/com/devsouzx/ecommerce/controllers/ProductController.java
+++ src/main/java/com/devsouzx/ecommerce/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package com.devsouzx.ecommerce.controllers;
 
+import com.devsouzx.ecommerce.domain.product.Category;
 import com.devsouzx.ecommerce.domain.product.Product;
+import com.devsouzx.ecommerce.dtos.CategoryRequestDTO;
+import com.devsouzx.ecommerce.dtos.CategoryResponseDTO;
 import com.devsouzx.ecommerce.dtos.ProductRequestDTO;
 import com.devsouzx.ecommerce.dtos.ProductResponseDTO;
 import com.devsouzx.ecommerce.services.CategoryService;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -32,5 +36,27 @@ public class ProductController {
         Product product = new Product(body.name(), body.description(), body.price(), body.stockQuantity(), body.categoryId(), LocalDateTime.now());
         productService.save(product);
         return ResponseEntity.ok(new ProductResponseDTO(product, categoryService.findById(body.categoryId())));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable UUID id) {
+        Product product = productService.findById(id);
+        return ResponseEntity.ok(new ProductResponseDTO(product, categoryService.findById(product.getCategoryId())));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable UUID id) {
+        Product product = productService.findById(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productService.delete(product);
+        return ResponseEntity.ok("Product deleted!");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable UUID id, @RequestBody ProductRequestDTO body) {
+        Product product = productService.update(id, body);
+        return ResponseEntity.ok(new ProductResponseDTO(product, categoryService.findById(product.getCategoryId())));
     }
 }
--- src/main/java/com/devsouzx/ecommerce/services/ProductService.java
+++ src/main/java/com/devsouzx/ecommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.devsouzx.ecommerce.services;
 
 import com.devsouzx.ecommerce.domain.product.Product;
+import com.devsouzx.ecommerce.dtos.ProductRequestDTO;
 import com.devsouzx.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,27 @@ public class ProductService {
 
     public List<Product> findProductsByCategoryId(UUID id) {
         return productRepository.findByCategoryId(id);
+    }
+
+    public Product findById(UUID id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found!"));
+    }
+
+    public void delete(Product product) {
+        productRepository.delete(product);
+    }
+
+    public Product update(UUID id, ProductRequestDTO body) {
+        Product product = this.findById(id);
+        this.updateDate(product, body);
+        return productRepository.save(product);
+    }
+
+    private void updateDate(Product product, ProductRequestDTO body) {
+        product.setName(body.name());
+        product.setDescription(body.description());
+        product.setPrice(body.price());
+        product.setStockQuantity(body.stockQuantity());
+        product.setCategoryId(body.categoryId());
     }
 }
